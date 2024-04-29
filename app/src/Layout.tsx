f0266b2c4bcbed5e678fe997ef6e5b9683324106
FishTub <alif.nurrokh@gmail.com>
2024-04-29 16:30:36 +1200

    removed unused routes + components

--- app/src/Layout.tsx
+++ app/src/Layout.tsx
@@ -1,13 +1,11 @@
 import { Box } from "@mui/material";
 import Footer from "./components/Footer/copyright";
 import Banner from "./components/Banner/banner";
-import Menu from "./components/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
       <Box> 
         <Banner />
-        {/* <Menu /> */}
         {children}
         <Footer />
       </Box>
